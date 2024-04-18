e8fdf04e36bd9864e24bea75b7c93e07a5e09ec1
nullboi <nullboi@tuta.io>
2024-04-18 18:39:59 -0400

    test remove sway

--- .config/sway/scripts/sleep.sh
+++
@@ -1,3 +0,0 @@
-swayidle -w timeout 300 'swaylock -f -i ~/imgs/walls/cute-poison.png' \
-            timeout 600 'systemctl suspend' \
-            before-sleep 'swaylock -f -i ~/imgs/walls/cute-poison.png' &
